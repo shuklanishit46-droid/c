89c22348bdd691420c45c0d7cef25e37ef3cb77a
shuklanishit46-droid <shuklanishit46@gmail.com>
2025-08-12 10:46:20 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float i,j;
+printf("Enter no1=");
+scanf("%f",&i);
+printf("Enter no2=");
+scanf("%f",&j);
+printf("Addition is=%f",i+j);
+printf("\nSubstraction is=%f",i-j);
+printf("\nMultiplication is=%f",i*j);
+printf("\nDivison is=%f",i/j);
+}
+
+getch();
