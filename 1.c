5e8cbdd44a1921c4dc6505b9e841a1b3bf927062
shuklanishit46-droid <shuklanishit46@gmail.com>
2025-08-12 09:57:03 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,ans;
+printf("Enter no1=");
+scanf("%d",&i);
+printf("Enter no2=");
+scanf("%d",&j);
+ans=i+j;
+printf("Addition of two numbers=%d",ans);
+}
+getch();
