9f0a835e8b508cc751115b2257d1bc4116f9db89
shuklanishit46-droid <shuklanishit46@gmail.com>
2025-08-12 10:26:40 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float i,j,ans;
+printf("Enter no1=");
+scanf("%f",&i);
+printf("Enter no2=");
+scanf("%f",&j);
+ans=i/j;
+printf("Ans of divison is=%f",ans);
+}
+getch();
