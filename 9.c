1d24ed5c09739741086f6ec149ff1f861b28212f
shuklanishit46-droid <shuklanishit46@gmail.com>
2025-08-19 10:32:00 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float r,d;
+printf("Enter amount in Rupees=");
+scanf("%f",&r);
+d=r/48;
+printf("The amount in Dollars=%.2f $",d);
+getch();
+}
